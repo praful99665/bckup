5b06a17fd829e02ee6a4fe5d300cc3b7988c321c
Praful <praful99665@gmail.com>
2026-02-02 15:20:39 +0530

    test

--- bckup/bckup/doctype/two_wheeler/two_wheeler.js
+++ bckup/bckup/doctype/two_wheeler/two_wheeler.js
@@ -1,8 +1,9 @@
 // Copyright (c) 2025, praful and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Two Wheeler", {
-// 	refresh(frm) {
+frappe.ui.form.on("Two Wheeler", {
+	refresh(frm) {
+        frappe.msgprint("Najiya")
 
-// 	},
-// });
+	},
+});
